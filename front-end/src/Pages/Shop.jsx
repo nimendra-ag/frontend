fdbadb808b41313f788f41d6c6a48a77e72cb53b
nimendra-ag <nimendraamiththa1@gmail.com>
2024-04-15 21:01:45 +0530

    include the html structure of the hero.jsx page. (without css) everything is work up to video time 37.49

--- front-end/src/Pages/Shop.jsx
+++ front-end/src/Pages/Shop.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
 
 const Shop = () => {
-  return (
-    <div>Shop</div>
-  )
+    return (
+        <div><Hero /></div>
+    )
 }
 
 export default Shop
