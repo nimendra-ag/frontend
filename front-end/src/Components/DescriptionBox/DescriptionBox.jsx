b3cd11be0279202bd9192e17528ccde4a128d5ba
nimendra-ag <nimendraamiththa1@gmail.com>
2024-05-06 09:14:50 +0530

    Create the description box. All is well upto 2.17.44

---
+++ front-end/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Odio iste debitis magni vitae recusandae ullam
+                    eveniet delectus at ipsum provident autem sit, rerum
+                    nostrum placeat eos aspernatur dolorem accusantium ea
+                    amet, a cum perferendis aut molestiae? Provident
+                    voluptates consequuntur excepturi dolorum, cum dolore?
+                    Ab voluptatem odit repellat nulla velit voluptatum?</p>
+                <p>
+                
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
