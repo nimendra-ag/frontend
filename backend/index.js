2a7e050ee01e0b193bc06657adbc3c4175cef5f0
nimendra-ag <nimendraamiththa1@gmail.com>
2024-05-07 12:39:26 +0530

    all is well upto 5.47.00

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const path = require("path");
 
 
 //API Creation    
-app.get("/",(req, res)=>{
+app.get("/", (req, res) => {
     res.send("Hello World");
 })
 
@@ -24,23 +24,85 @@ app.get("/",(req, res)=>{
 
 const storage = multer.diskStorage({
     destination: './upload/images',
-    filename: (req, file, cb)=>{
-        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
     }
 })
 
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 
 //Creating upload endpoint for images
 app.use('/images', express.static('upload/images'))
 
-app.post("/upload", upload.single('product'), (req, res)=>{
+app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:3001/images/${req.file.filename}`})
+        image_url: `http://localhost:3001/images/${req.file.filename}`
+    })
 })
 
-app.listen(3001, ()=>{
+
+//Schema for Creating Products
+
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    new_price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: String,
+        default: Date.now
+    },
+    available: {
+        type: Boolean,
+        default: true
+    }
+});
+
+app.post('/addproduct', async (req, res) => {
+    let products = await Product.find({});
+    let id;
+    if (products.length > 0) {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id + 1;
+    } else {
+        id = 1;
+    }
+    const product = new Product({
+        id: id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    });
+    console.log(product)
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success: true,
+        name: req.body.name,
+    })
+})
+
+app.listen(3001, () => {
     console.log('Server is running on port 3001');
 })
