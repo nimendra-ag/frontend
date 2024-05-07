967d67dde825a0083824e2a0cf2f8ee7e1e17f66
nimendra-ag <nimendraamiththa1@gmail.com>
2024-05-07 10:38:56 +0530

    server is running. installed nodemon as well. all is well upto 5.15.00

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,22 @@
-const port = 4000;
 const express = require("express")
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// const UserModel
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// const jwt = require("jsonwebtoken")
+// const multer = require("multer");
+// const path = require("path");
+
+app.get("/",(req, res)=>{
+    res.send("Hello World");
+})
+
+
+app.listen(3001, ()=>{
+    console.log('Server is running on port 3001');
+})
