730686ad9986a818a3be1582454aeef880ace930
nimendra-ag <nimendraamiththa1@gmail.com>
2024-05-07 11:53:58 +0530

    image uploading endpoint created. all is well upto 5.28.03

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,38 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const jwt = require("jsonwebtoken")
-// const multer = require("multer");
-// const path = require("path");
 
+mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
+// const jwt = require("jsonwebtoken")
+const multer = require("multer");
+const path = require("path");
+
+
+//API Creation    
 app.get("/",(req, res)=>{
     res.send("Hello World");
 })
 
+//Image Storage Engine
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage: storage});
+
+
+//Creating upload endpoint for images
+app.use('/images', express.static('upload/images'))
+
+app.post("/upload", upload.single('product'), (req, res)=>{
+    res.json({
+        success: 1,
+        image_url: `http://localhost:3001/images/${req.file.filename}`})
+})
 
 app.listen(3001, ()=>{
     console.log('Server is running on port 3001');
