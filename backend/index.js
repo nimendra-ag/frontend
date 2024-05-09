6b1819326ec780c2c191e70dc963e20566750ae1
nimendra-ag <nimendraamiththa1@gmail.com>
2024-05-10 01:12:02 +0530

    all is well upto 8.50.10

--- backend/index.js
+++ backend/index.js
@@ -214,6 +214,43 @@ app.get('/newcollections', async(req, res)=>{
     res.send(newcollection);
 })
 
+//creating endpoint for popular in women section
+app.get('/popularinwomen', async(req, res)=>{
+    let products = await Product.find({
+        category:"women"
+    });
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women fetched")
+    res.send(popular_in_women);
+})
+
+//creating middelware to fetch user
+const fetchUser = async (req, res, next)=>{
+    const token = req.header('auth-token');
+    if (!token){
+        res.status(401).send({errors: "Please authenticate a using valid token"})
+    }
+    else{
+        try{
+            const data = jwt.verify(token, 'secret_ecom');
+            req.user = data.user;
+            next();
+        }catch(error){
+            res.status(401).send({errors:"Please authenticate using a valid token."})
+        }
+    }
+
+}
+
+//creating endpoint for adding products in cartdata
+app.post('/addtocart', fetchUser, async(req, res)=>{{
+    let userData = await Users.findOne({_id:req.user.id})
+    userData.cartData[req.body.itemId] += 1;
+    await Users.findOneAndUpdate({_id:req.user.id}, {cartData:userData.cartData});
+    res.send("Added")
+}})
+
+
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
 })
